791a9b865c86fe0dc64b7a2c932d5b2689a42666
imdipo <imammuhammaddiponegoro@gmail.com>
2026-03-17 14:05:06 +0700

    hasil nguli nguli

--- Scraping/Scraping_ArtikelBerita/config.py
+++ Scraping/Scraping_ArtikelBerita/config.py
@@ -9,3 +9,17 @@ RANDOM_KATA = [
     "cantik",
     "ganteng",
 ]
+
+MAP_PORTAL = {
+   "cnnindonesia": {"container": "div", "class": "detail-text"},
+   "indozone": {"container": "div", "class": "article-ct"},
+   "mkompas": {"container": "div", "class": "clearfix"},
+   "detik": {"container": "div", "class": "detail__body-text"},
+   "republika": {"container": "div", "class": "article-content"},
+   "liputan6": {"container": "div", "class": "article-content-body__item-content"},
+   "tribunnews": {"container": "div", "class": "side-article"},
+
+
+   "kumparan": {"container": "main", "attrs": {"data-qa-id": "article-main"}}
+
+}
