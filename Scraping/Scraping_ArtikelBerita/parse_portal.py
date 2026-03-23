92e617c863190e842e0e4748805d4842c51b6c82
imdipo <imammuhammaddiponegoro@gmail.com>
2026-03-23 22:57:46 +0700

    matiin buat debugging nya

--- Scraping/Scraping_ArtikelBerita/parse_portal.py
+++ Scraping/Scraping_ArtikelBerita/parse_portal.py
@@ -1,6 +1,6 @@
 from urllib.parse import urlparse
 from config import MAP_PORTAL
-# link = "https://ekonomi.republika.co.id/berita/tc0ts8490/hadapi-investigasi-dagang-dari-as-airlangga-fokus-isu-kapasitas-produksi-dan-kerja-paksa-part2?page=all"
+# link = "https://megapolitan.kompas.com/sitemap.xml"
 
 """
 beberapa berita punya variasi nama tergantung fokus berita
@@ -11,12 +11,14 @@ jadi ini buat ngambil hanya A nya aja, soalnya kita butuh buat bandingin sama ke
 
 def ambil_portal(url):
     homepage = urlparse(url).netloc
-    print(homepage)
+    # print(homepage)
 
     nama_web = homepage.split(".")
-    print(nama_web)
+    # print(nama_web)
 
     nama_portal = [web for web in nama_web if web in MAP_PORTAL]
     nama = nama_portal[0]
 
     return nama
+
+# ambil_portal(link)
