fcdec4a2d04fe7b987b550c82acc3462ece750dc
imdipo <imammuhammaddiponegoro@gmail.com>
2026-03-23 22:58:18 +0700

    ganti cara ngaambil nama portal berita nya

--- Scraping/Scraping_ArtikelBerita/main.py
+++ Scraping/Scraping_ArtikelBerita/main.py
@@ -1,5 +1,5 @@
 from urllib.parse import urlparse
-
+from parse_portal import ambil_portal
 from scraping_links import getting_all_link
 from scraping_contents import harvest_informations
 from utils import save_txt
@@ -14,7 +14,7 @@ dan hasilnya keluar, itu udah lumayan efisien
 """
 
 def main(homepage):
-    nama_portal = urlparse(homepage).netloc
+    nama_portal = ambil_portal(homepage)
 
     urls = getting_all_link(homepage)
 
