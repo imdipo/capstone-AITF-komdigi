f6b70a7facf2742cf50080a3b934b54ddd883958
imdipo <imammuhammaddiponegoro@gmail.com>
2026-03-26 16:02:53 +0700

    make it more robust if met CDATA somthin somthin

--- Scraping/Scraping_ArtikelBerita/scraping_links.py
+++ Scraping/Scraping_ArtikelBerita/scraping_links.py
@@ -15,7 +15,7 @@ def getting_all_link(homepage, link_terkumpul = None):
         link_terkumpul = set()
     
     try: 
-        response = requests.get(homepage, headers=HEADERS, timeout=15)
+        response = requests.get(homepage, headers=HEADERS, timeout=20)
         soup = BeautifulSoup(response.content, 'xml')
         
         sitemap_lain = soup.find_all('sitemap')
@@ -26,12 +26,22 @@ def getting_all_link(homepage, link_terkumpul = None):
 
             # ini kita ambil yang 2025 keatas, sebenernya bebas sih kalau mau ambil yang 2024+ atau berapapun. cuman tadi sempet tes yang 2023 beritanya udah ga ada
             for sitemap in sitemap_lain:
-                sub_urls = sitemap.find('loc').text.strip()
-                lastmod = sitemap.find('lastmod')
+                loc_tag = sitemap.find('loc')
+                print(loc_tag)
+                raw_text = loc_tag.get_text(strip=True)
+                clean_url = raw_text.replace("<![CDATA[", "").replace("]]>", "")
+                sub_urls = clean_url.strip()
 
+# sub_urls.replace("\n", "").replace("\t", "").strip()
+                lastmod = sitemap.find('lastmod')
                 if lastmod:
-                    tanggal = lastmod.text.strip()
+                    raw_tanggal = lastmod.get_text(strip=True)
+                    clean_tanggal = raw_tanggal.replace("<![CDATA[", "").replace("]]>", "")
+                    tanggal = clean_tanggal.strip()
+
+                    print(f"tanggal-1 {tanggal}")
                     tahun = int(tanggal[:4])
+                    print(f"tanggal-2 {tanggal}")
                     if tahun >= 2025:
                         getting_all_link(sub_urls, link_terkumpul)
 
@@ -42,9 +52,16 @@ def getting_all_link(homepage, link_terkumpul = None):
         if urls:
             print(f"okei, udah di page file link. ada sebanyak {len(urls)} link disini {biar_ga_bosen()}")
             for link in urls:
-                link_berita = link.find('loc').text.strip().replace("\n", "").replace("\t", "")
+                # print(link)
+                loc_tag = link.find('loc')
+                link_berita_raw = loc_tag.get_text()
+                link_berita_bersih = link_berita_raw.replace("<![CDATA[", "").replace("]]>", "")
+                link_berita = link_berita_bersih.strip()
+                # print(f"jvebvje {link_berita}")
+
                 link_berita = link_berita + "?page=all" # ntah work atau ngga wkwk, cuman pada bisa sih ya
-                link_terkumpul.add(link_berita) # pake set dulu sebelum txt biar ga ada duplikat    
+                link_terkumpul.add(link_berita) # pake set dulu sebelum txt biar ga ada duplikat  
+        # else: print("ga ada somehow")  
     
 
     except Exception as e:
