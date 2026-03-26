c30ca7556a1fb0e972df43fd3a4f3deaebbbfa2d
imdipo <imammuhammaddiponegoro@gmail.com>
2026-03-26 16:02:25 +0700

    update

--- Scraping/Scraping_ArtikelBerita/config.py
+++ Scraping/Scraping_ArtikelBerita/config.py
@@ -15,11 +15,14 @@ MAP_PORTAL = {
    "indozone": {"tag": "div", "spec": {"class": "article-ct"}},
    "kompas": {"tag": "div", "spec": {"class": "read__content"}},
    "detik": {"tag": "div", "spec":{"class": "detail__body-text"}},
-   "republika": {"tag": "div", "spec": {"class": "article-content"}},
    "liputan6": {"tag": "div", "spec": {"class": "article-content-body__item-content"}},
-   "tribunnews": {"tag": "div", "spec": {"class": "side-article"}},
-
+   "cnbcindonesia": {"tag": "div", "spec": {"class": "detail-text"}},
+   "okezone": {"tag": "div", "spec":{"class" : "c-detail read"}},
 
    "kumparan": {"tag": "main", "spec":{"data-qa-id": "article-main"}}
+
+   # "jpnn": {"tag": "div", "spec": {"itemprop": "articleBody"}},
+#    "republika": {"tag": "div", "spec": {"class": "article-content"}}, # dia ga ada sitemap ternyata. bisa pake dari crawler(gagal).py tapi belum kurapihin 
+   # "tribunnews": {"tag": "div", "spec": {"class": "side-article"}}, # ketahan waf. nanti kapan kapan aku belajar dulu 
 
 }
